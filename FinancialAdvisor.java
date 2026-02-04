473bb240ddfb147fb079fac47131dfeedd4b022f
Alexander Hanna <144834431+hannaa3atwit@users.noreply.github.com>
2026-02-04 13:41:12 -0500

    Create FinancialAdvisor entity class
    This class represents a financial advisor entity with fields for personal information and a list of clients.

---
+++ FinancialAdvisor.java
@@ -0,0 +1,55 @@
+package com.example.<YOUR_PACKAGE>.entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class FinancialAdvisor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long advisorId;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column
+    private String phone;
+
+    @OneToMany(mappedBy = "advisor", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Client> clients;
+
+    public FinancialAdvisor(Long advisorId, String firstName, String lastName, String email, String phone, List<Client> clients) {
+        this.advisorId = advisorId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.clients = clients;
+    }
+
+    public FinancialAdvisor() {}
+
+    public Long getAdvisorId() { return advisorId; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public List<Client> getClients() { return clients; }
+    public void setClients(List<Client> clients) { this.clients = clients; }
+}
